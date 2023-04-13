9d4e197da9dccdc9c023c3dc8797ff92258ddeb3
Simon Plenderleith <simon@simonplend.co.uk>
2023-04-13 14:01:13 -0700

    [frontend] Create login and logout buttons

--- blog-frontend/src/components/layout.js
+++ blog-frontend/src/components/layout.js
@@ -1,6 +1,9 @@
 import { useAuth0 } from '@auth0/auth0-react'
 import Link from 'next/link'
 
+import LoginButton from '@/components/login'
+import LogoutButton from '@/components/logout'
+
 export default function Layout({ children }) {
   const { user, error, isAuthenticated, isLoading } = useAuth0()
 
@@ -20,6 +23,18 @@ export default function Layout({ children }) {
         <h1>
           <Link href="/">My blog</Link>
         </h1>
+        <section>
+          {isAuthenticated ? (
+            <>
+              <p>
+                <strong>Logged in as {user.name}</strong>
+              </p>
+              <LogoutButton />
+            </>
+          ) : (
+            <LoginButton />
+          )}
+        </section>
       </header>
       <main>
         {children}
