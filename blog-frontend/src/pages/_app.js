852e5192e4942acd3c6c13b322c032ac229d77d6
Simon Plenderleith <simon@simonplend.co.uk>
2023-04-13 12:25:43 -0700

    [frontend] Integrate the Auth0 React SDK

--- blog-frontend/src/pages/_app.js
+++ blog-frontend/src/pages/_app.js
@@ -1,5 +1,25 @@
 import '@/styles/globals.css'
 
+import { Auth0Provider } from '@auth0/auth0-react'
+import Router from 'next/router'
+
+const onRedirectCallback = (appState) => {
+  Router.replace(appState?.returnTo || '/');
+}
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Auth0Provider
+      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN}
+      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
+      onRedirectCallback={onRedirectCallback}
+      authorizationParams={{
+        redirect_uri: typeof window !== 'undefined' && window.location.origin,
+        audience: process.env.NEXT_PUBLIC_AUTH0_AUDIENCE,
+        scope: process.env.NEXT_PUBLIC_AUTH0_SCOPE
+      }}
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
+  )
 }
