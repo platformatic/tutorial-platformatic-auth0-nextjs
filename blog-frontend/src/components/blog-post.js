730cc33c23dd26530de5217db6aa1004b1db8014
Simon Plenderleith <simon@simonplend.co.uk>
2023-04-13 23:40:06 -0700

    [frontend] Create the edit post page

--- blog-frontend/src/components/blog-post.js
+++ blog-frontend/src/components/blog-post.js
@@ -1,4 +1,11 @@
+import { useAuth0 } from '@auth0/auth0-react'
+import Link from 'next/link'
+
 export default function BlogPost({ id, title, body, authorUserId, authorName, createdAt, updatedAt }) {
+  const { user } = useAuth0()
+
+  const currentUserIsAuthor = user?.sub === authorUserId
+
   return (
     <div>
       <h2>{title}</h2>
@@ -10,6 +17,7 @@ export default function BlogPost({ id, title, body, authorUserId, authorName, cr
         <em>By {authorName}</em>
       </p>
       <p>{body}</p>
+      {currentUserIsAuthor && <Link href={`/edit/${id}`}>Edit</Link>}
     </div>
   )
 }
